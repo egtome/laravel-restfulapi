8736fc435a02bc063262f7604762011815c782c5
Gino Tome <ginotome@gmail.com>
2020-04-04 11:33:27 -0300

    First commit

---
+++ routes/api.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+#BUYERS
+Route::resource('buyers', 'Buyer\BuyerController', ['only' => ['index','show']]);
+#CATEGORIES
+Route::resource('categories', 'Category\CategoryController', ['except' => ['create','edit']]);
+#PRODUCTS
+Route::resource('products', 'Product\ProductController', ['only' => ['index','show']]);
+#SELLERS
+Route::resource('sellers', 'Seller\SellerController', ['only' => ['index','show']]);
+#TRANSACTIONS
+Route::resource('transactions', 'Transaction\TransactionController', ['only' => ['index','show']]);
+#USERS
+Route::resource('users', 'User\UserController', ['except' => ['create','edit']]);
+
